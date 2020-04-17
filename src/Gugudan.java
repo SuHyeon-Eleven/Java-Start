8a0968cda973b29feaa4229bcfa226e90266f192
MSH <MSH@DESKTOP-BSKEU0A>
2020-04-17 12:11:08 +0900

    구구단 단 입력 & 출력

---
+++ src/Gugudan.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+public class Gugudan {
+
+	public static void main(String[] args) {
+		
+//		System.out.println("2의 배수");
+//		System.out.println(2 * 1);
+//		System.out.println(2 * 2);
+//		System.out.println(2 * 3);
+//		System.out.println(2 * 4);
+//		System.out.println(2 * 5);
+//		System.out.println(2 * 6);
+//		System.out.println(2 * 7);
+//		System.out.println(2 * 8);
+//		System.out.println(2 * 9);
+//
+//		System.out.println("3의 배수");
+//		System.out.println(3 * 1);
+//		System.out.println(3 * 2);
+//		System.out.println(3 * 3);
+//		System.out.println(3 * 4);
+//		System.out.println(3 * 5);
+//		System.out.println(3 * 6);
+//		System.out.println(3 * 7);
+//		System.out.println(3 * 8);
+//		System.out.println(3 * 9);
+//		
+		//4단
+		System.out.println("4단");
+		int result = 4 * 1;
+		System.out.println(result);
+		result = 4 * 2;
+		System.out.println(result);
+		result = 4 * 3;
+		System.out.println(result);
+		result = 4 * 4;
+		System.out.println(result);
+		result = 4 * 5;
+		System.out.println(result);
+		result = 4 * 6;
+		System.out.println(result);
+		result = 4 * 7;
+		System.out.println(result);
+		result = 4 * 8;
+		System.out.println(result);
+		result = 4 * 9;
+		System.out.println(result);
+	
+		
+		System.out.print("원하는 단을 입력하세요  > ");
+
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		result = num * 1;
+		System.out.println(result);
+		
+		result = num * 2;
+		System.out.println(result);
+		
+		result = num * 3;
+		System.out.println(result);
+		
+		result = num * 4;
+		System.out.println(result);
+		
+		result = num * 5;
+		System.out.println(result);
+		
+		result = num * 6;
+		System.out.println(result);
+		
+		result = num * 7;
+		System.out.println(result);
+		
+		result = num * 8;
+		System.out.println(result);
+		
+		result = num * 9;
+		System.out.println(result);
+		
+	}
+
+}
